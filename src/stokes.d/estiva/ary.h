f75a50c20a88e1ec6a98826954732092701fb62b
TSUKUDA Yoshio <tsukuda.yoshio@gmail.com>
2016-06-13 23:12:26 +0900

    .gitignore

--- src/stokes.d/estiva/ary.h
+++ src/stokes.d/estiva/ary.h
@@ -7,7 +7,7 @@ size_t  estiva_dim0(void* v);
 long    estiva_dim1(void* v);
 long    estiva_dim2(void* v);
 
-#define ary1(b,n_1)        estiva_ary1((void *)&b,n_1,sizeof(*b))
+#define ary1(b,n_1)        estiva_ary1((void **)&b,n_1,sizeof(*b))
 #define ary2(A,m_1,n_1)    estiva_ary2((void *)&A,m_1,n_1,sizeof(**A))
 #define dim0(v)            estiva_dim0(v)
 #define dim1(v)            estiva_dim1(v)
