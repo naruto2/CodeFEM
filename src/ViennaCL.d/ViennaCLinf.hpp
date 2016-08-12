9eeeae52ac631b5ffdda643359abfb6a3f2b60ff
naruto2 <tsukuda.yoshio@gmail.com>
2016-08-12 21:42:01 +0900

    bicgtab inf ka.

--- src/ViennaCL.d/ViennaCLinf.hpp
+++ src/ViennaCL.d/ViennaCLinf.hpp
@@ -4,6 +4,7 @@
 
 #include <viennacl/linalg/jacobi_precond.hpp>
 #include "cginf.hpp"
+#include "bicgstabinf.hpp"
 #include "togpu.hpp"
 
 vector<double> gpucg(matrix& A, vector<double>& b)
@@ -20,6 +21,27 @@ vector<double> gpucg(matrix& A, vector<double>& b)
   viennacl::linalg::cg_tag  custom_cg(1e-5,1000000);
 
   xgpu = viennacl::linalg::solve(Agpu, bgpu, custom_cg, vcl_jacobi);
+
+  gpuvector2vector(xgpu,x);
+
+  return x;
+}
+
+
+vector<double> gpubicgstab(matrix& A, vector<double>& b)
+{
+  int n = A.size();
+  vector<double> x(n);
+  viennacl::compressed_matrix<double> Agpu(n,0);
+  viennacl::vector<double>     bgpu(n), xgpu(n);
+
+  matrix2gpumatrix(A,Agpu);
+  vector2gpuvector(b,bgpu);
+
+  viennacl::linalg::jacobi_precond< gpumatrix >  vcl_jacobi(Agpu,viennacl::linalg::jacobi_tag());
+  viennacl::linalg::bicgstab_tag  custom_bicgstab(1e-5,1000000);
+
+  xgpu = viennacl::linalg::solve(Agpu, bgpu, custom_bicgstab, vcl_jacobi);
 
   gpuvector2vector(xgpu,x);
 
