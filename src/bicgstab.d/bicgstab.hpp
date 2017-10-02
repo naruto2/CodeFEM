2117c5709e8b578a0fcc935879380666800f1507
naruto2 <tsukuda.yoshio@gmail.com>
2017-10-02 21:06:07 +0900

    Yahoo!, bicgstab() is now free from sparse::matrix<double>.

--- src/bicgstab.d/bicgstab.hpp
+++ src/bicgstab.d/bicgstab.hpp
@@ -19,111 +19,196 @@
 //  
 //*****************************************************************
 
-int 
-sparse__BiCGSTAB(const sparse::matrix<double> &A, double *x, double *b,
-		 int &max_iter, double &tol)
+static double norm(int n, double *x)
 {
-  int k, n = A.size();
-  double tmp;
-  tmp = 0.0;
-  for (k=1;k<n;k++) tmp += b[k]*b[k];
-  double tmq,resid,rho_1,rho_2,alpha,beta,omega, normb = sqrt(tmp);
+  double tmp=0.0;
+  for (int k=1;k<n;k++) tmp += x[k]*x[k];
+  return sqrt(tmp);
+}
 
-  static double *r,*p,*phat,*s,*shat,*t,*v,*rtilde;
-  if (!r) r = (double*)calloc(n,sizeof(double));
-  if (!p) p = (double*)calloc(n,sizeof(double));
-  if (!s) s = (double*)calloc(n,sizeof(double));
-  if (!t) t = (double*)calloc(n,sizeof(double));
-  if (!v) v = (double*)calloc(n,sizeof(double));
-  if (!phat) phat = (double*)calloc(n,sizeof(double));
-  if (!shat) shat = (double*)calloc(n,sizeof(double));
-  if (!rtilde) rtilde = (double*)calloc(n,sizeof(double));
-  
-  for (k=1;k<n;k++ ) {
+
+static void copy(int n, double *p, double *q)
+{
+  for (int k=1;k<n;k++) p[k]=q[k];
+}
+
+
+static double dot(int n, double *p, double *q)
+{
+  double tmp=0.0;
+  for (int k=1; k<n; k++ ) tmp += p[k]*q[k];
+  return tmp;
+}
+
+static double phase0(int n, double *r, const sparse::matrix<double>&A,
+		     double *x, double *rtilde, double *b)
+{
+  double tmp=0.0;
+  for (int k=1;k<n;k++ ) {
     r[k] = 0.0;
     for (auto j : A[k] ) r[k] += j.second * x[j.first];
+    rtilde[k] = r[k] = b[k] - r[k];
+    tmp += r[k]*r[k];
   }
-  for(k=1;k<n;k++) rtilde[k] = r[k] = b[k] - r[k];
+  return sqrt(tmp);
+}
 
+
+static void phase1(int n, double *p, double *r, double *v,
+		   double beta, double omega)
+{
+  for(int k=1;k<n;k++) p[k] = r[k] + beta * (p[k] - omega *v[k]);
+}
+
+static double phase2(int n, double *v, const sparse::matrix<double>A,
+		     double *Aa, int *col_ind, int *row_ptr,
+		     double *phat, double *rtilde)
+{
+  double tmp=0.0;
+#if 1
+  for (int k=1;k<n;k++ ) {
+    v[k] = 0.0;
+    for (int j=row_ptr[k];j<row_ptr[k+1];j++){
+      v[k] += Aa[j] * phat[col_ind[j]];
+    }
+    tmp += rtilde[k]*v[k];
+  }
+#endif  
+#if 0
+  for (int k=1;k<n;k++ ) {
+    v[k] = 0.0;
+    int jj = row_ptr[k];
+    for (auto j : A[k] ) {
+      printf("col_ind[jj]=%d j=%d\n",col_ind[jj],j.first);
+      v[k] += j.second * phat[j.first];
+    }
+    tmp += rtilde[k]*v[k];
+  }
+#endif
+  return tmp;
+}
+
+
+static void phase3(int n, double *s, double * r, double * v, double alpha)
+{
+  for(int k=1;k<n;k++) s[k] = r[k] - alpha * v[k];
+}
+
+
+static void phase4(int n, double *x, double *phat, double alpha)
+{
+  for(int k=1;k<n;k++) x[k] = x[k] + alpha*phat[k];
+}
+
+
+static double phase5(int n, double *t, const sparse::matrix<double>&A,
+		     double *shat, double *s)
+{
+  double tmp=0.0, tmq=0.0;
+  for (int k=1;k<n;k++ ) {
+    t[k] = 0.0;
+    for (auto j : A[k] ) t[k] += j.second * shat[j.first];
+    tmp += t[k]*s[k];
+    tmq += t[k]*t[k];
+  }
+  return tmp/tmq;
+}
+
+static void phase6(int n, double *x, double *s, double *r, double *t,
+		   double *phat, double *shat, double alpha, double omega)
+{
+  for(int k=1;k<n;k++) {
+    x[k] = x[k] + alpha*phat[k] + omega*shat[k];
+    r[k] = s[k] - omega * t[k];
+  }
+}
+    
+
+int sparse__BiCGSTAB(const sparse::matrix<double> &A, double *x, double *b,
+		     int &max_iter, double &tol)
+{
+  static int nn, k, ww, w, *row_ptr, *col_ind, n = A.size();
+  static double *r,*p,*phat,*s,*shat,*t,*v,*rtilde, *Aa;
+
+  if ( nn < n ) { free(r); free(row_ptr); r=0; }
+  if (!r) {
+    r = (double*)malloc(8*n*sizeof(double));
+    p = &r[n]; s = &r[2*n]; t = &r[3*n]; v = &r[4*n];
+    phat = &r[5*n]; shat = &r[6*n]; rtilde = &r[7*n];
+    row_ptr = (int*)malloc((n+1)*sizeof(int));
+    nn = n;
+  }
+
+  w = 1;
+  for ( k=1;k<n;k++ ) {
+    row_ptr[k] = w;
+    for (auto j : A[k] ) if(j.second != 0.0) w++;
+  }
+  row_ptr[n] = w;
+  for(k=0;k<=n;k++)printf("row_ptr[%d]=%d\n",k,row_ptr[k]);
+  printf("w=%d\n",w);
+
+  if( ww < w ) { free(Aa); free(col_ind); Aa=0; }
+  if (!Aa){
+    Aa      = (double*)malloc((w+1)*sizeof(double));
+    col_ind = (int   *)malloc((w+1)*sizeof(int));
+    ww = w;
+  }
+  int ii=1;
+  for ( k=1;k<n;k++ ) 
+    for (auto j : A[k] ) if(j.second != 0.0) {
+	Aa[ii] = j.second;
+	col_ind[ii] = j.first;
+	ii++;
+      }
+
+#if 0
+  for ( k=1;k<w;k++) printf("col_ind[%d]=%d Aa[%d]=%f\n",
+			     k,col_ind[k],k,Aa[k]);
+#endif  
+  double resid,rho_1,rho_2,alpha,beta,omega, normb = norm(n,b);
   if (normb == 0.0) normb = 1;
-  tmp=0.0;
-  for (k=1;k<n;k++) tmp += r[k]*r[k];  
-  if ((resid = sqrt(tmp) / normb) <= tol) {
+
+  if ((resid = phase0(n,r,A,x,rtilde,b)/normb) <= tol) {
     tol = resid;
     max_iter = 0;
     return 0;
   }
-
   for (int i = 1; i <= max_iter; i++) {
-
-    rho_1=0.0;
-    for (k=1; k<n; k++ ) rho_1 += rtilde[k]*r[k];
-
+    rho_1 = dot(n,rtilde,r);
     if (rho_1 == 0) {
-      tmp=0.0;
-      for (k=1;k<n;k++) tmp += r[k]*r[k];
-      tol = sqrt(tmp) / normb;
+      tol = norm(n,r)/normb;
       return 2;
     }
     if (i == 1)
-      for(k=1;k<n;k++) p[k]=r[k];
+      copy(n,p,r);
     else {
       beta = (rho_1/rho_2) * (alpha/omega);
-      for(k=1;k<n;k++) p[k] = r[k] + beta * (p[k] - omega *v[k]);
+      phase1(n,p,r,v,beta,omega);
     }
-    for(k=1;k<n;k++) phat[k] = p[k];
-
-    for (k=1;k<n;k++ ) {
-      v[k] = 0.0;
-      for (auto j : A[k] ) v[k] += j.second * phat[j.first];
-    }
-
-    tmp=0.0;
-    for (k=1;k<n;k++) tmp += rtilde[k]*v[k];
-    alpha = rho_1 / tmp;
-
-    for(k=1;k<n;k++) s[k] = r[k] - alpha * v[k];
-
-    tmp=0.0;
-    for (k=1;k<n;k++) tmp += s[k]*s[k];
-    if ((resid = sqrt(tmp)/normb) < tol) {
-      for(k=1;k<n;k++) x[k] = x[k] + alpha*phat[k];
+    copy(n,phat,p);
+    alpha = rho_1/phase2(n,v,A,Aa,col_ind,row_ptr,phat,rtilde);
+    phase3(n,s,r,v,alpha);
+    
+    if ((resid = norm(n,s)/normb) < tol) {
+      phase4(n,x,phat,alpha);
       tol = resid;
       return 0;
     }
-    for(k=1;k<n;k++) shat[k] = s[k];
-
-    for (k=1;k<n;k++ ) {
-      t[k] = 0.0;
-      for (auto j : A[k] ) t[k] += j.second * shat[j.first];
-    }
-    tmp=0.0;
-    for (k=1;k<n;k++) tmp += t[k]*s[k];
-    tmq=0.0;
-    for (k=1;k<n;k++) tmq += t[k]*t[k];
-    omega = tmp / tmq;
-
-    for(k=1;k<n;k++) {
-      x[k] = x[k] + alpha*phat[k] + omega*shat[k];
-      r[k] = s[k] - omega * t[k];
-    }
+    copy(n,shat,s);
+    omega = phase5(n,t,A,shat,s);
+    phase6(n,x,s,r,t,phat,shat,alpha,omega);
     rho_2 = rho_1;
-
-    tmp=0.0;
-    for (k=1;k<n;k++) tmp += r[k]*r[k];
-    if ((resid = sqrt(tmp) / normb) < tol) {
+    if ((resid = norm(n,r)/normb) < tol) {
       tol = resid;
       max_iter = i;
       return 0;
     }
     if (omega == 0) {
-      tmp=0.0;
-      for (k=1;k<n;k++) tmp += r[k]*r[k];
-      tol = sqrt(tmp) / normb;
+      tol = norm(n,r)/normb;
       return 3;
     }
   }
-
   tol = resid;
   return 1;
 }
