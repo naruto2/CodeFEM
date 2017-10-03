a451674089486dee70bbf238d67b05c5c0f11872
naruto2 <tsukuda.yoshio@gmail.com>
2017-10-03 17:10:35 +0900

    cl_init() and cl_finalize are created.

--- src/dataparallel.d/main.cpp
+++ src/dataparallel.d/main.cpp
@@ -1,21 +1,6 @@
 #include <stdlib.h>
-#ifdef __APPLE__
-#include <OpenCL/opencl.h>
-#else
 #include <CL/cl.h>
-#endif
 #include <stdio.h>
-
-#define NAME_NUM (8)  /* Number of stocks */
-#define DATA_NUM (21) /* Number of data to process for each stock */
-
-/* Read Stock data */
-int stock_array_many[NAME_NUM*DATA_NUM];
-
-
-
-/* Moving average width */
-#define WINDOW_SIZE (13)
 
 #define MAX_SOURCE_SIZE (0x100000)
 
@@ -25,7 +10,7 @@ static cl_uint work_dim = 1;
 static size_t global_item_size[3];
 static size_t local_item_size[3];
 static cl_program program = NULL;
-
+static char *kernel_src_str;
 
 static float cl_norm(int nn, float *x_norm)
 {
@@ -35,7 +20,7 @@ static float cl_norm(int nn, float *x_norm)
 
   if(!kernel_norm) {
     kernel_norm = clCreateKernel(program, "mynorm", &ret);
-    printf("kernel_norm=%d\n",ret);
+    printf("cl_norm=%d\n",ret);
   }
   if(!mem_x_norm)
     mem_x_norm  = clCreateBuffer(context, CL_MEM_READ_WRITE,
@@ -127,49 +112,31 @@ static float cl_dot(int nn, float *y_copy, float *x_norm)
     return x_norm[0];
 }
 
-
-
-int main(int argc, char **argv)
+void cl_init(int argc, char **argv)
 {
-    cl_platform_id platform_id = NULL;
-    cl_uint ret_num_platforms;
-    cl_device_id device_id = NULL;
-    cl_uint ret_num_devices;
 
-    cl_mem memobjIn = NULL;
-    cl_mem memobjIn1 = NULL;
-    cl_mem memobjIn2 = NULL;
-    cl_mem memobjOut = NULL;
-    cl_mem memobjOut1 = NULL;
+  if (kernel_src_str != NULL ) return;
+  cl_platform_id platform_id = NULL;
+  cl_uint ret_num_platforms;
+  cl_device_id device_id = NULL;
+  cl_uint ret_num_devices;
+  size_t kernel_code_size;
+  cl_int ret;
+  FILE *fp;
+    
+    /* Set parameters for data parallel processing (work item) */
 
-    cl_kernel kernel = NULL;
-    size_t kernel_code_size;
-    char *kernel_src_str;
-    int *result;
-    float *z, *x, *y;
-    cl_int ret;
-    FILE *fp;
-    int k;
+    global_item_size[0] = 4; /* np Global number of work items */
+    local_item_size[0] = 1;  /* Number of work items per work group */
 
-    int window_num = (int)WINDOW_SIZE;
-    int point_num = NAME_NUM * DATA_NUM;
-    int data_num = (int)DATA_NUM;
-    int name_num = (int)NAME_NUM;
+    if ( argc > 1 ) global_item_size[0] = atoi(argv[1]);    
+    
+    /* --> global_item_size[0] / local_item_size[0] becomes 2, which indirectly sets the number of workgroups to 2*/
 
-    int i,j;
 
     /* Allocate space to read in kernel code */
     kernel_src_str = (char *)malloc(MAX_SOURCE_SIZE);
-
-    /* Allocate space for the result on the host side */
-    result = (int *)malloc(point_num*sizeof(int));
-    z = (float *)malloc(8192*sizeof(float));
-    x = (float *)malloc(8192*sizeof(float));
-    y = (float *)malloc(8192*sizeof(float));
-
-    for(k=0;k<8192;k++) x[k] = 1.0;
-    for(k=0;k<8192;k++) y[k] = 1.0;
-
+    
     /* Get Platform*/
     ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
 
@@ -195,95 +162,37 @@ int main(int argc, char **argv)
     /* Compile kernel */         
     ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
 
-    /* Create kernel */
-    kernel = clCreateKernel(program, "moving_average_vec4_para", &ret);
+}
 
-    /* Create buffer for the input data on the device */
-    memobjIn  = clCreateBuffer(context, CL_MEM_READ_WRITE,
-                               point_num * sizeof(int), NULL, &ret);
 
-    /* Create buffer for the result on the device */    
-    memobjOut = clCreateBuffer(context, CL_MEM_READ_WRITE,
-                               point_num * sizeof(float), NULL, &ret);
-
-    /* Copy input data to the global memory on the device*/
-    ret = clEnqueueWriteBuffer(command_queue, memobjIn, CL_TRUE, 0,
-                               point_num * sizeof(int),
-                               stock_array_many, 0, NULL, NULL);
-
-    memobjIn1  = clCreateBuffer(context, CL_MEM_READ_WRITE,
-				8192 * sizeof(float), NULL, &ret);
-    memobjIn2  = clCreateBuffer(context, CL_MEM_READ_WRITE,
-				8192 * sizeof(float), NULL, &ret);
-    memobjOut1 = clCreateBuffer(context, CL_MEM_READ_WRITE,
-				8192 * sizeof(float), NULL, &ret);
-
-    ret = clEnqueueWriteBuffer(command_queue, memobjIn1, CL_TRUE, 0,
-                               8192 * sizeof(float),
-                               x, 0, NULL, NULL);
-    ret = clEnqueueWriteBuffer(command_queue, memobjIn2, CL_TRUE, 0,
-                               8192 * sizeof(float),
-                               y, 0, NULL, NULL);
-
-    
-    /* Set Kernel Arguments */
-    ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&memobjIn);
-    ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&memobjOut);
-    ret = clSetKernelArg(kernel, 2, sizeof(int),    (void *)&data_num);
-    ret = clSetKernelArg(kernel, 3, sizeof(int),    (void *)&name_num);
-    ret = clSetKernelArg(kernel, 4, sizeof(int),    (void *)&window_num);
-    ret = clSetKernelArg(kernel, 5, sizeof(cl_mem), (void *)&memobjIn1);
-    ret = clSetKernelArg(kernel, 6, sizeof(cl_mem), (void *)&memobjIn2);
-    ret = clSetKernelArg(kernel, 7, sizeof(cl_mem), (void *)&memobjOut1);
-
-    /* Set parameters for data parallel processing (work item) */
-
-    global_item_size[0] = 4; /* np Global number of work items */
-    local_item_size[0] = 1;  /* Number of work items per work group */
-
-    if ( argc > 1 ) global_item_size[0] = atoi(argv[1]);    
-    
-    /* --> global_item_size[0] / local_item_size[0] becomes 2, which indirectly sets the number of workgroups to 2*/
-
-    /* Execute Data Parallel Kernel */ 
-    ret = clEnqueueNDRangeKernel(command_queue, kernel, work_dim, NULL,
-                                 global_item_size, local_item_size,
-                                 0, NULL, NULL);
-
-    /* Copy result from device to host */
-    ret = clEnqueueReadBuffer(command_queue, memobjOut, CL_TRUE, 0,
-                              point_num * sizeof(int),
-                              result, 0, NULL, NULL);
-
-    ret = clEnqueueReadBuffer(command_queue, memobjOut1, CL_TRUE, 0,
-                              8192 * sizeof(float),
-                              z, 0, NULL, NULL);
-    ret = clReleaseKernel(kernel);
-    ret = clReleaseMemObject(memobjIn);
-    ret = clReleaseMemObject(memobjOut);
-    ret = clReleaseMemObject(memobjOut1);
-
-#ifndef mynorm // mynorm用 ################################################## 
-    int nn = 1024;
-    float *x_norm = (float*)calloc(sizeof(float),nn);
-    x_norm[0] = 0.0;
-    for(k=1;k<nn;k++) x_norm[k] = 1.0;
-    printf("norm(n,x) = %f\n",cl_norm(nn,x_norm));
-
-    float *y_copy = (float*)calloc(sizeof(float),nn);
-    cl_copy(nn,y_copy,x_norm);
-    printf("dot(n,y,x) = %f\n",cl_dot(nn,y_copy,x_norm));
-#endif // #####################################################################
+void cl_finalize(void)
+{
     /* OpenCL Object Finalization */
-
+    int ret;
     ret = clReleaseProgram(program);
     ret = clReleaseCommandQueue(command_queue);
     ret = clReleaseContext(context);
 
-    printf("np=%d\n",result[0]);    
     /* Deallocate memory on the host */
-    free(result);
     free(kernel_src_str);
-    return 0;
 }
 
+
+int main(int argc, char **argv)
+{
+  cl_init(argc,argv);
+
+  int k, n = 65537;
+  float *x = (float*)malloc(sizeof(float)*n);
+  float *y = (float*)malloc(sizeof(float)*n);
+
+  x[0] = 0.0; for(k=1;k<n;k++) x[k] = 1.0;
+
+  printf("norm(n,x) = %f\n",cl_norm(n,x));
+
+  cl_copy(n,y,x);
+  printf("dot(n,y,x) = %f\n",cl_dot(n,y,x));
+
+  cl_finalize();
+  return 0;
+}
--- src/dataparallel.d/moving_average_vec4_para.cl
+++ src/dataparallel.d/moving_average_vec4_para.cl
@@ -1,16 +1,3 @@
-__kernel void moving_average_vec4_para(__global int4  *values,
-                                       __global int *out,
-                                       int length,
-                                       int name_num,
-                                       int width,
-				       __global float *x,
-				       __global float *y,
-				       __global float *z)
-{
-  out[0] = get_global_size(0);
-}
-
-
 __kernel void mynorm(int n,__global float *x)
 {
   int k, rank = get_global_id(0);
